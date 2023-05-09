e556dc7c65bd97f7bef6f3728f123d295133b645
Nikhil Joji <nikhiljoji07@gmail.com>
2023-05-09 21:58:03 +0530

    Revert "Tabshot"

--- background.js
+++ background.js
@@ -6,23 +6,34 @@ chrome.contextMenus.create({
   
   chrome.browserAction.onClicked.addListener(function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id,{toggle:1});
+        chrome.tabs.sendMessage(tabs[0].id,"toggle");
     })
 });
 
 function clickHandler(e) {
     
-    chrome.tabs.captureVisibleTab(null, {}, function (image) {
-        // console.log(image); 
-        chrome.storage.local.set({'img':image}, function() {});
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+    if(e.mediaType === "image")
         {
-            chrome.tabs.sendMessage(tabs[0].id,{toggle:1});
+            chrome.storage.sync.set({'img': e.srcUrl}, function() {
+            });
+
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id,"toggle");
+            });
+
+
+        
+        }
+
+    if(e.selectionText)
+    {
+        chrome.storage.sync.set({'refer': e.selectionText}, function() {
         });
 
-     });
-
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,"toggle");
+        });
+    }
 
 
 }
-
